004ec0f3f8fc4978498b5577efc00c22f77ceab4
Muhammet Yavuz <muhammetyavuz846@gmail.com>
2024-02-12 23:23:15 +0300

    görünüm visibiltyleri ayarlandı

--- app/src/main/java/com/example/recipeapplication/profileFragment.kt
+++ app/src/main/java/com/example/recipeapplication/profileFragment.kt
@@ -7,9 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.recipeapplication.databinding.FragmentProfileBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class profileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,10 +30,17 @@ class profileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth=Firebase.auth
+        val currentUser=auth.currentUser
+        if (currentUser!=null){
+            binding.hesapAcilmamisGorunum.visibility=View.GONE
+            binding.girisYaptKtanSonrakiGoruntu.visibility=View.VISIBLE
+        }
         binding.girisYapUyeOlbutonu.setOnClickListener {
             val intent=Intent(context,loginActivity::class.java)
             startActivity(intent)
         }
+
     }
 
 
