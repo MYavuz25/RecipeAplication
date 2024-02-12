0e0dfa5cd601e2d1b4c7fc780f48d184b097367b
Muhammet Yavuz <muhammetyavuz846@gmail.com>
2024-02-12 21:08:56 +0300

    üye olma ve giriş yapma foksiyonları tanımlandı

--- app/src/main/java/com/example/recipeapplication/MainActivity.kt
+++ app/src/main/java/com/example/recipeapplication/MainActivity.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.recipeapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var navHostFragment: NavHostFragment?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        
+
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         navHostFragment.let {
             NavigationUI.setupWithNavController(binding.bottomNavigationView,it!!.navController)
         }
+
 
     }
 }
--- app/src/main/java/com/example/recipeapplication/loginActivity.kt
+++ app/src/main/java/com/example/recipeapplication/loginActivity.kt
@@ -1,28 +1,68 @@
 package com.example.recipeapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.recipeapplication.databinding.ActivityLoginBinding
+import com.google.common.base.MoreObjects.ToStringHelper
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class loginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        auth=Firebase.auth
 
     }
     fun girisYap(view: View){
         //giris yapma kodları
-        Toast.makeText(this,"giriş Yap",Toast.LENGTH_LONG).show()
+        val email=binding.emailEditText.text.toString()
+        val password=binding.passwordEditText.text.toString()
+        if (email.isNotEmpty()&&password.isNotEmpty()){
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+                //Success
+
+                val intent=Intent(this@loginActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                //Fail
+                Toast.makeText(this@loginActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }else{
+            Toast.makeText(this@loginActivity,"Enter an E-mail and password!",Toast.LENGTH_LONG).show()
+        }
+
+
     }
     fun uyeOl(view: View){
         //üye olma işlemleri yapılacak
-        Toast.makeText(this,"uye ol",Toast.LENGTH_LONG).show()
+
+        val email=binding.emailEditText.text.toString()
+        val password=binding.passwordEditText.text.toString()
+
+        if (email.isNotEmpty()&& password.isNotEmpty()){
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                //success
+                val intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                //Fail
+                Toast.makeText(this@loginActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }else{
+            Toast.makeText(this@loginActivity,"Enter an E-mail and password!",Toast.LENGTH_LONG).show()
+        }
+
 
     }
 }
