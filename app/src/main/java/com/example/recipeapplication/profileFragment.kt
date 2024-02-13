e9f1bae95ac7ea4b528be55d7da62107bf560a5a
Muhammet Yavuz <muhammetyavuz846@gmail.com>
2024-02-13 16:22:23 +0300

    sign in ve sing out işlemleri stabil ve kullanılır hale getirildi

--- app/src/main/java/com/example/recipeapplication/profileFragment.kt
+++ app/src/main/java/com/example/recipeapplication/profileFragment.kt
@@ -17,15 +17,20 @@ class profileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
+
     ): View? {
         binding=FragmentProfileBinding.inflate(inflater,container,false)
         return binding.root
+
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -40,8 +45,15 @@ class profileFragment : Fragment() {
             val intent=Intent(context,loginActivity::class.java)
             startActivity(intent)
         }
+        binding.singOutButton.setOnClickListener {
+            auth.signOut()
+            binding.hesapAcilmamisGorunum.visibility=View.VISIBLE
+            binding.girisYaptKtanSonrakiGoruntu.visibility=View.GONE
+        }
+
 
     }
+
 
 
 }
