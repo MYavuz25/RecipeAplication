525abfb9d42cf4c1be640a229573710014a663d2
Muhammet Yavuz <muhammetyavuz846@gmail.com>
2024-02-13 18:18:06 +0300

    recipe add fragmentına menüyle erişimi sağlandı

--- app/src/main/java/com/example/recipeapplication/profileFragment.kt
+++ app/src/main/java/com/example/recipeapplication/profileFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.recipeapplication.databinding.FragmentProfileBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -50,6 +51,7 @@ class profileFragment : Fragment() {
             auth.signOut()
             binding.hesapAcilmamisGorunum.visibility=View.VISIBLE
             binding.girisYaptKtanSonrakiGoruntu.visibility=View.GONE
+            Toast.makeText(context,"Çıkış Yapıldı",Toast.LENGTH_SHORT).show()
         }
 
 
